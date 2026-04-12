11943e726ed9ccf69411f2feafe4d1acaed712bc
RafaelVendramini <rafaelbvc@hotmail.com>
2026-04-12 06:04:18 -0300

    v-0.1.2 - Refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
 import { Footer } from './containers/footer/Footer';
 import { Header } from './containers/header/Header';
+import { Inicio } from './pages/inicio/Inicio';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Inicio />
       <Footer />
     </div>
   );
