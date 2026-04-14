c81242da8d02dc670004662b5df308fa5fe88a20
RafaelVendramini <rafaelbvc@hotmail.com>
2026-04-14 17:58:07 -0300

    v-0.2.6 - Carousel

--- src/pages/midia/Midia.tsx
+++ src/pages/midia/Midia.tsx
@@ -1,20 +1,29 @@
-import "./MidiaCSS.css"
-import MidiaIMG from "../../assets/backgroundPages/midiaIMG.jpg"
 import { FC } from "react"
+import "./MidiaCSS.css"
+import Carousel from "../../components/carousel/Carousel"
+import MidiaIMG from "../../assets/backgroundPages/midiaIMG.jpg"
+import InicioIMG from "../../assets/backgroundPages/inicioIMG.jpg"
+import PainelIMG from "../../assets/backgroundPages/painelIMG.jpg"
+import ContatoIMG from "../../assets/backgroundPages/contatoIMG.jpg"
+
 
 
 export const Midia: FC = () => {
 
-    return (
+    const myImages = [PainelIMG, ContatoIMG, MidiaIMG, InicioIMG]
 
-        <div style={{
+    return (
+        <div className="midia-container">
+            {/* <div style={{
             backgroundImage: `url(${MidiaIMG})`,
             width: "100vw",
             height: "100vh",
             backgroundSize: "cover",
             backgroundRepeat: "no-repeat",
             backgroundPosition: "center"
-        }}>
+        }}>*/}
+
+            <Carousel images={myImages} />
 
         </div>
 
