f705dd5c04b9f700fc4ef3c71c569fdf6c4ab4aa
RafaelVendramini <rafaelbvc@hotmail.com>
2026-04-14 14:03:50 -0300

    v-0.2.3 - Inicio Responsivity

--- src/pages/inicio/Inicio.tsx
+++ src/pages/inicio/Inicio.tsx
@@ -20,7 +20,13 @@ export const Inicio = () => {
                 <span className="span-conteudo-um">Confiança</span>
                 <span className="span-conteudo-um">Luxo</span>
             </div>
-            <div className="inicio-conteudo-dois"></div>
+            <div className="inicio-conteudo-dois">
+                <h3 className="htres-conteudo-dois">Marco Personal Driver:<br/>Excelência em cada trajeto</h3>
+                <p className="p-conteudo-dois">Ofereço serviços de transporte executivo com foco em pontualidade, segurança e discrição</p>
+                <p className="p-conteudo-dois">Meu veículo é de alto padrão, conduzido por motorista treinado para atender agendas exigentes</p>
+                <p className="p-conteudo-dois">Seja para transfer de aeroportos, eventos corporativos, shows, consultas, ou logística diária, 
+                                               garanto um deslocamento eficiente e confortável para quem não pode perder tempo</p>
+            </div>
         </div>
     );
 };
