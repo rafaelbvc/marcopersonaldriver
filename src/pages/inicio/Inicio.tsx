5cc64f9d063b040c70a5ee85d3176ef70fbf66f8
RafaelVendramini <rafaelbvc@hotmail.com>
2026-04-15 19:34:15 -0300

    v-0.3.1 - Clamp and Text Changes

--- src/pages/inicio/Inicio.tsx
+++ src/pages/inicio/Inicio.tsx
@@ -14,19 +14,18 @@ export const Inicio: FC = () => {
             backgroundRepeat: "no-repeat",
         }}>
             <div className="inicio-conteudo-um">
-                <span className="span-conteudo-um">Segurança</span>
-                <span className="span-conteudo-um">Conforto</span>
-                <span className="span-conteudo-um">Rapidez</span>
-                <span className="span-conteudo-um">Disponibilidade</span>
-                <span className="span-conteudo-um">Confiança</span>
-                <span className="span-conteudo-um">Luxo</span>
+                <span className="span-conteudo-um">SEGURANÇA</span>
+                <span className="span-conteudo-um">CONFORTO</span>
+                <span className="span-conteudo-um">RAPIDEZ</span>
+                <span className="span-conteudo-um">DISPONIBILIDADE</span>
+                <span className="span-conteudo-um">CONFIANÇA</span>
+                <span className="span-conteudo-um">LUXO</span>
             </div>
             <div className="inicio-conteudo-dois">
-                <h3 className="htres-conteudo-dois">Marco Personal Driver:<br/>Excelência em cada trajeto</h3>
-                <p className="p-conteudo-dois">Ofereço serviços de transporte executivo com foco em pontualidade, segurança e discrição</p>
-                <p className="p-conteudo-dois">Meu veículo é de alto padrão, conduzido por motorista treinado para atender agendas exigentes</p>
-                <p className="p-conteudo-dois">Seja para transfer de aeroportos, eventos corporativos, shows, consultas, ou logística diária, 
-                                               garanto um deslocamento eficiente e confortável para quem não pode perder tempo</p>
+                <h3 className="htres-conteudo-dois">MARCO PERSONAL DRIVER:<br/>EXCELÊNCIA EM CADA TRAJETO</h3>
+                <p className="p-conteudo-dois">OFEREÇO SERVIÇOS DE TRANSPORTE EXECUTIVO COM FOCO EM PONTUALIDADE, SEGURANÇA E DISCRIÇÂO</p>
+                <p className="p-conteudo-dois">SEJA PARA TRANSFER DE AEPORTOS, EVENTOS CORPORATIVOS, SHOWS, CONSULTAS, OU LOGÍSTICA DIÁRIA, 
+                                               GARANTO UM DESLOCAMENTO EFICIENTE E CONFORTÁVEL PARA QUEM NÃO PODE PERDER TEMPO</p>
             </div>
         </div>
     );
